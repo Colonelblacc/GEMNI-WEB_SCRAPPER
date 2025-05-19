07c0d631c88ab0764d5749cf937dc995618cd110
Colonelblacc <colonelblackshawn@gmail.com>
2025-05-20 00:10:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,5 +29,5 @@ if "dom_content" in st.session_state:
             st.write("Parsing the content")
 
             dom_chunks=split_dom_content(st.session_state.dom_content)
-            result=parse_with_ollama(dom_chunks,parse_description)
+            result=parse_with_gemini(dom_chunks,parse_description)
             st.write(result)
